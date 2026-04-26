30a4dc177326e9e08425351fe7fded9b4c4963bb
Muhammad-Rezka <rezkaapasha29@gmail.com>
2026-04-26 23:28:16 +0800

    Tugas Pertemuan 7

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,30 @@ Route::get('blog', function () {
 Route::get('Pert5', function () {
     return view('Pertemuan5');
 });
+Route::get('Pert1', function () {
+    return view('Pertemuan1');
+});
+Route::get('Pert2', function () {
+    return view('Pertemuan2');
+});
+Route::get('Pert3', function () {
+    return view('Pertemuan3');
+});
+Route::get('TPert3', function () {
+    return view('Tugaspert3');
+});
+Route::get('Pert4', function () {
+    return view('Pertemuan4');
+});
+Route::get('Pert5index', function () {
+    return view('Pertemuan5index');
+});
+Route::get('Tugaspert5', function () {
+    return view('TugasLinkTree');
+});
+Route::get('Pert6', function () {
+    return view('Pertemuan6');
+});
 
 Route::get('dosen', [DosenController::class, 'index']);
 Route::get('biodata', [DosenController::class, 'biodata']);
